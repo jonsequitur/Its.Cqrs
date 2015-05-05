73fca33e1539775c37dddd5a68a949d86618bc5a
yuanfeiw <yuanfeiw@microsoft.com>
2015-05-04 20:03:59 -0700

    EF6 code based configuration

---
+++ Domain.Sql/EfDbConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace Microsoft.Its.Domain.Sql
+{
+    public class EfDbConfiguration : DbConfiguration
+    {
+        public EfDbConfiguration()
+        {
+            SetProviderServices("System.Data.SqlClient", SqlProviderServices.Instance);
+        }
+    }
+}
