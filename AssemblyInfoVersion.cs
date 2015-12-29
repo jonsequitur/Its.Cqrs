ed9a6f4a1d565f258a01207ca5b7e1f32413f2be
jonsequitur <jonsequeira@gmail.com>
2015-12-28 18:07:42 -0800

    version 0.13.0-beta

--- AssemblyInfoVersion.cs
+++ AssemblyInfoVersion.cs
@@ -5,9 +5,9 @@ using System;
 using System.Reflection;
 
 // Don't edit this version.
-[assembly: AssemblyVersion("0.12.0.0")]
+[assembly: AssemblyVersion("0.13.0.0")]
 [assembly: CLSCompliant(true)]
 
 // Edit these for each release + update dependencies in .nuspec files
-[assembly: AssemblyFileVersion("0.12.12-beta")]
-[assembly: AssemblyInformationalVersion("0.12.12-beta")]
+[assembly: AssemblyFileVersion("0.13.0-beta")]
+[assembly: AssemblyInformationalVersion("0.13.0-beta")]
