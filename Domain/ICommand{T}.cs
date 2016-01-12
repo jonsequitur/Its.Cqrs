62543cc907005455fffcc86be6d789e0c18376eb
Phillip Pruett <phpruett@microsoft.com>
2016-01-12 13:34:49 -0800

    fix doc comments to specify target not aggregate. fix spacing in csproj

--- Domain/ICommand{T}.cs
+++ Domain/ICommand{T}.cs
@@ -8,17 +8,17 @@ using Its.Validation;
 namespace Microsoft.Its.Domain
 {
     /// <summary>
-    ///     A command that can be applied to an aggregate to trigger some action and record an applicable state change.
+    ///     A command that can be applied to an target to trigger some action and record an applicable state change.
     /// </summary>
-    /// <typeparam name="T">The type of the aggregate.</typeparam>
+    /// <typeparam name="T">The type of the target.</typeparam>
     public interface ICommand<in T> : ICommand
     {
         /// <summary>
-        ///     Performs the action of the command upon the aggregate.
+        ///     Performs the action of the command upon the target.
         /// </summary>
-        /// <param name="target">The aggregate to which to apply the command.</param>
+        /// <param name="target">The target to which to apply the command.</param>
         /// <exception cref="CommandValidationException">
-        ///     If the command cannot be applied due its state or the state of the aggregate, it should throw a
+        ///     If the command cannot be applied due its state or the state of the target, it should throw a
         ///     <see
         ///         cref="CommandValidationException" />
         ///     indicating the specifics of the failure.
@@ -26,11 +26,11 @@ namespace Microsoft.Its.Domain
         void ApplyTo(T target);
 
         /// <summary>
-        ///     Performs the action of the command upon the aggregate.
+        ///     Performs the action of the command upon the target.
         /// </summary>
-        /// <param name="target">The aggregate to which to apply the command.</param>
+        /// <param name="target">The target to which to apply the command.</param>
         /// <exception cref="CommandValidationException">
-        ///     If the command cannot be applied due its state or the state of the aggregate, it should throw a
+        ///     If the command cannot be applied due its state or the state of the target, it should throw a
         ///     <see
         ///         cref="CommandValidationException" />
         ///     indicating the specifics of the failure.
@@ -38,7 +38,7 @@ namespace Microsoft.Its.Domain
         Task ApplyToAsync(T target);
 
         /// <summary>
-        ///     Gets a validator that can be used to check the valididty of the command against the state of the aggregate before it is applied.
+        ///     Gets a validator that can be used to check the valididty of the command against the state of the target before it is applied.
         /// </summary>
         IValidationRule<T> Validator { get; }
     }
