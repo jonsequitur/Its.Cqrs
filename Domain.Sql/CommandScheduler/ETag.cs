ca57e5ea4bf1e681c4c90834c469b552444804e5
jonsequitur <jonsequeira@gmail.com>
2016-01-19 10:21:45 -0800

    add ETag table to command scheduler db

---
+++ Domain.Sql/CommandScheduler/ETag.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft. All rights reserved. 
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.Its.Domain.Sql.CommandScheduler
+{
+    public class ETag
+    {
+        public Guid AggregateId { get; set; }
+
+        public string ETagValue { get; set; }
+
+        public DateTimeOffset CreatedTime { get; set; } 
+    }
+}
