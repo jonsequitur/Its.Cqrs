2b5de762f1a082401b5889fcdf2868b71d5d9767
Jon Sequeira <jonsequeira@gmail.com>
2016-03-14 15:14:01 -0700

    version 0.14.1-beta

---
+++ AssemblyVersion.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft. All rights reserved. 
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Reflection;
+
+// Don't edit this version.
+[assembly: AssemblyVersion("0.14.0.0")]
+[assembly: CLSCompliant(true)]
+
+// Edit these for each release + update dependencies in .nuspec files
+[assembly: AssemblyInformationalVersion("0.14.1-beta")]
