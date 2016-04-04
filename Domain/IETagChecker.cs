7f62d577133ed88ec1b2a6869f56f31914987dd3
jonsequitur <jonsequeira@gmail.com>
2016-04-04 07:37:26 -0700

    doc comments

--- Domain/IETagChecker.cs
+++ Domain/IETagChecker.cs
@@ -7,10 +7,15 @@ using System.Threading.Tasks;
 namespace Microsoft.Its.Domain
 {
     /// <summary>
-    /// Verifies whether a precondition has been satisfied.
+    /// Verifies whether an etag has been recorded.
     /// </summary>
     public interface IETagChecker
     {
+        /// <summary>
+        /// Determines whether the specified etag has been recorded within the specified scope.
+        /// </summary>
+        /// <param name="scope">The scope within which the etag is unique.</param>
+        /// <param name="etag">The etag.</param>
         Task<bool> HasBeenRecorded(string scope, string etag);
     }
 }
