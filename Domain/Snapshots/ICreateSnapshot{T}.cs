6409c9d48143e07c5b8ec0045dc5b75cd6c48eb5
jonsequitur <jonsequeira@gmail.com>
2016-04-04 07:35:44 -0700

    add doc comments

--- Domain/Snapshots/ICreateSnapshot{T}.cs
+++ Domain/Snapshots/ICreateSnapshot{T}.cs
@@ -5,9 +5,17 @@ using System;
 
 namespace Microsoft.Its.Domain
 {
+    /// <summary>
+    /// Creates snapshots from aggregate instances.
+    /// </summary>
+    /// <typeparam name="TAggregate">The type of the aggregate.</typeparam>
     public interface ICreateSnapshot<in TAggregate>
         where TAggregate : class, IEventSourced
     {
+        /// <summary>
+        /// Creates a snapshot from the specified aggregate.
+        /// </summary>
+        /// <param name="aggregate">The aggregate.</param>
         ISnapshot CreateSnapshot(TAggregate aggregate);
     }
 }
