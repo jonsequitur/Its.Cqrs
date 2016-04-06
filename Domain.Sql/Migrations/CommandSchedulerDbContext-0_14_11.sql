b5f6e4b423dd86f8c86f41f307e4c0bb5a18c438
jonsequitur <jonsequeira@gmail.com>
2016-04-05 19:00:07 -0700

    change command scheduler db to have a clustered index and non-clustered pk

---
+++ Domain.Sql/Migrations/CommandSchedulerDbContext-0_14_11.sql
@@ -0,0 +1,37 @@
+IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name='IX_Scope_and_ETag' AND object_id = OBJECT_ID('Scheduler.ETag'))
+BEGIN
+	CREATE UNIQUE NONCLUSTERED INDEX [IX_Scope_and_ETag] ON [Scheduler].[ETag]
+    (
+    	[Scope] ASC,
+    	[ETagValue] ASC
+    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+END
+
+IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name='IX_Temp' AND object_id = OBJECT_ID('Scheduler.ScheduledCommand'))
+BEGIN
+	CREATE UNIQUE NONCLUSTERED INDEX [IX_Temp] on [Scheduler].[ScheduledCommand]
+	(
+	[AggregateId] ASC,
+	[SequenceNumber] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+	ALTER TABLE [Scheduler].[Error] DROP CONSTRAINT [FK_Scheduler.Error_Scheduler.ScheduledCommand_ScheduledCommand_AggregateId_ScheduledCommand_SequenceNumber]
+
+	ALTER TABLE [Scheduler].[ScheduledCommand] DROP CONSTRAINT [PK_Scheduler.ScheduledCommand]
+
+	ALTER TABLE [Scheduler].[ScheduledCommand] ADD  CONSTRAINT [PK_Scheduler.ScheduledCommand] PRIMARY KEY NONCLUSTERED 
+	(
+	[AggregateId] ASC,
+	[SequenceNumber] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+	DROP INDEX [IX_Temp] on [Scheduler].[ScheduledCommand]
+	
+	CREATE CLUSTERED INDEX [IX_AppliedTime_Clock_Id_FinalAttemptTime_DueTime] ON 
+	[Scheduler].[ScheduledCommand] 
+		([AppliedTime], 
+		 [Clock_Id], 
+		 [FinalAttemptTime], 
+		 [DueTime]) 
+	WITH (ONLINE = OFF)
+END
