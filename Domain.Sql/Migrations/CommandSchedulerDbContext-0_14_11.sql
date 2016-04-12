49dbdac6413959e9fdf5046604ea1fa0e7b91fd2
jonsequitur <jonsequeira@gmail.com>
2016-04-12 08:41:51 -0700

    make error table foreign key removal idempotent

--- Domain.Sql/Migrations/CommandSchedulerDbContext-0_14_11.sql
+++ Domain.Sql/Migrations/CommandSchedulerDbContext-0_14_11.sql
@@ -7,15 +7,24 @@ BEGIN
     )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 END
 
+
+IF EXISTS (SELECT * FROM sys.foreign_keys
+           WHERE object_id = (SELECT object_id 
+           FROM sys.objects 
+           WHERE name = 'FK_Scheduler.Error_Scheduler.ScheduledCommand_ScheduledCommand_AggregateId_ScheduledCommand_SequenceNumber')) 
+BEGIN
+	ALTER TABLE [Scheduler].[Error] 
+	DROP CONSTRAINT [FK_Scheduler.Error_Scheduler.ScheduledCommand_ScheduledCommand_AggregateId_ScheduledCommand_SequenceNumber]
+END
+
+
 IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name='IX_Temp' AND object_id = OBJECT_ID('Scheduler.ScheduledCommand'))
 BEGIN
 	CREATE UNIQUE NONCLUSTERED INDEX [IX_Temp] on [Scheduler].[ScheduledCommand]
 	(
 	[AggregateId] ASC,
 	[SequenceNumber] ASC
-	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-
-	ALTER TABLE [Scheduler].[Error] DROP CONSTRAINT [FK_Scheduler.Error_Scheduler.ScheduledCommand_ScheduledCommand_AggregateId_ScheduledCommand_SequenceNumber]
+	) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 
 	ALTER TABLE [Scheduler].[ScheduledCommand] DROP CONSTRAINT [PK_Scheduler.ScheduledCommand]
 
@@ -23,7 +32,7 @@ BEGIN
 	(
 	[AggregateId] ASC,
 	[SequenceNumber] ASC
-	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 
 	DROP INDEX [IX_Temp] on [Scheduler].[ScheduledCommand]
 	
