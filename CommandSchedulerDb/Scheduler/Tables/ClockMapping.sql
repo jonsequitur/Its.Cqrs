d3ebf4736a86c26d525c632425bdf92085af3e11
Xavier John <xavierjohn@hotmail.com>
2016-04-12 18:04:18 -0700

    CommandSchedulerDb

---
+++ CommandSchedulerDb/Scheduler/Tables/ClockMapping.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [Scheduler].[ClockMapping] (
+    [Id]       BIGINT         IDENTITY (1, 1) NOT NULL,
+    [Value]    NVARCHAR (128) NOT NULL,
+    [Clock_Id] INT            NOT NULL,
+    CONSTRAINT [PK_Scheduler.ClockMapping] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Scheduler.ClockMapping_Scheduler.Clock_Clock_Id] FOREIGN KEY ([Clock_Id]) REFERENCES [Scheduler].[Clock] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Clock_Id]
+    ON [Scheduler].[ClockMapping]([Clock_Id] ASC);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Value]
+    ON [Scheduler].[ClockMapping]([Value] ASC);
+
