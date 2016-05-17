880ee1a0e96f1bceb3fed9da9d6fbefccac995ea
jonsequitur <jonsequeira@gmail.com>
2016-05-17 08:59:05 -0700

    merge, bump version to 0.14.22-beta

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -9,4 +9,4 @@ using System.Reflection;
 [assembly: CLSCompliant(true)]
 
 // Edit these for each release + update dependencies in .nuspec files
-[assembly: AssemblyInformationalVersion("0.14.21-beta")]
+[assembly: AssemblyInformationalVersion("0.14.22-beta")]
