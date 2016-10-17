448b20562007fb83c83b49bd62fb5b93d6e376bb
jonsequitur <jonsequeira@gmail.com>
2016-10-17 15:45:52 -0700

    version 0.15.11-beta

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -9,4 +9,4 @@ using System.Reflection;
 [assembly: CLSCompliant(true)]
 
 // Edit these for each release + update dependencies in .nuspec files
-[assembly: AssemblyInformationalVersion("0.15.10-beta")]
+[assembly: AssemblyInformationalVersion("0.15.11-beta")]
