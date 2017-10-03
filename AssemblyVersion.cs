4b2a10876cf41edbe374e2de521246c061333bb2
KimberlyPhan <kimphan@gmail.com>
2017-10-03 16:10:31 -0700

    bump up version to 0.15.26

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -9,4 +9,4 @@ using System.Reflection;
 [assembly: CLSCompliant(true)]
 
 // Edit these for each release + update dependencies in .nuspec files
-[assembly: AssemblyInformationalVersion("0.15.25")]
+[assembly: AssemblyInformationalVersion("0.15.26")]
