2553bc9b8f009503c487677b1216254e9ad2b0c7
jonsequitur <jonsequeira@gmail.com>
2017-11-12 10:06:07 -0800

    make test not dependent on other tests having run first

--- Domain.Sql.Tests/DiagnosticTests.cs
+++ Domain.Sql.Tests/DiagnosticTests.cs
@@ -1,18 +1,24 @@
 // Copyright (c) Microsoft. All rights reserved. 
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
+using Test.Domain.Ordering;
 
 namespace Microsoft.Its.Domain.Sql.Tests
 {
     [TestFixture]
     [UseSqlEventStore]
-    public class DiagnosticTests 
+    public class DiagnosticTests : EventStoreDbTest
     {
         [Test]
-        public void Sensor_can_be_used_to_check_read_model_catchup_status()
+        public async Task Sensor_can_be_used_to_check_read_model_catchup_status()
         {
+            Events.Write(1);
+
+            CreateReadModelCatchup(Projector.Create<Order.Cancelled>(e => {})).Dispose();
+
             var result = Sensors.CatchupStatus().Result;
 
             ((long) result.LatestEventId).Should().BeGreaterOrEqualTo(0);
