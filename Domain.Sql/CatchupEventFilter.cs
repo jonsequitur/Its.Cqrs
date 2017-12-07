e44797ef6f47424a1b145d69889bf940b5581eb8
Xavier John <xavierjohn@hotmail.com>
2017-12-06 19:54:10 -0800

    Improve the CatchUp SQL to WHERE StreamName = 'xyz' and Type IN ('type1','type2')

--- Domain.Sql/CatchupEventFilter.cs
+++ Domain.Sql/CatchupEventFilter.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
+
 
 namespace Microsoft.Its.Domain.Sql
 {
@@ -22,25 +24,26 @@ namespace Microsoft.Its.Domain.Sql
 
         public Expression<Func<StorableEvent, bool>> Filter { get; }
 
-        private static BinaryExpression GetExpression(MatchEvent criteria, ParameterExpression p)
-        {
-            var streamNameMatches = Expression.Equal(
-                Expression.Property(p, "StreamName"),
-                Expression.Constant(criteria.StreamName));
-            var typeMatches = Expression.Equal(
-                Expression.Property(p, "Type"),
-                Expression.Constant(criteria.Type));
-            return Expression.AndAlso(streamNameMatches, typeMatches);
-        }
-
-        private static Expression<Func<StorableEvent, bool>> FilterForEventMatchCustomBuild(IEnumerable<MatchEvent> criteria)
+        private static Expression<Func<StorableEvent, bool>> FilterForEventMatchCustomBuild(IEnumerable<MatchEvent> criterias)
         {
             var seParam = Expression.Parameter(typeof(StorableEvent), "storableEvent");
+            var groupByStreamName = criterias.GroupBy(c => c.StreamName, c => c.Type);
 
-            var body = criteria.Select(c => GetExpression(c, seParam))
-                               .Aggregate(Expression.OrElse);
+            var body = groupByStreamName
+                .Select(c => GetExpression(c, seParam))
+                .Aggregate(Expression.OrElse);
 
             return Expression.Lambda<Func<StorableEvent, bool>>(body, seParam);
+        }
+
+        private static BinaryExpression GetExpression(IGrouping<string, string> criteria, ParameterExpression p)
+        {
+            var streamNameMatches = Expression.Equal(Expression.Property(p, "StreamName"), Expression.Constant(criteria.Key));
+
+            var method = typeof(Enumerable).GetRuntimeMethods().Single(m => m.Name == nameof(Enumerable.Contains) && m.GetParameters().Length == 2);
+            var containsMethod = method.MakeGenericMethod(typeof(string));
+            var typeMatches = Expression.Call(containsMethod, Expression.Constant(criteria.ToArray()), Expression.Property(p, "Type"));
+            return Expression.AndAlso(streamNameMatches, typeMatches);
         }
     }
 }
